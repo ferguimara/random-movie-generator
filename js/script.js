a97d026d7e059e7bf22bea7ee5a7134dfa93cbf0
Fernando Guimaraes <Fernando@Fernandos-MBP.lan>
2021-03-17 20:50:33 -0400

    basic code setup part 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// constant Variables - data that never changes
+
+// state variables - data that changes
+
+// cached element references - parts of the dom we need to touch
+
+//event listeners - capture and respond to events
+
+//functions -  code that represents actions taken
